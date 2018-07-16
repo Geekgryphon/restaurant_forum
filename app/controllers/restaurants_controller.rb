0dd0c54cd6897993de1bbe20bc85e0d82b725393
brian <geekgryphon@gmail.com>
2018-07-17 06:03:57 +0800

    complete restaurant index page for public users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,9 @@
 class RestaurantsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
+  
     def index
+      @restaurants = Restaurant.page(params[:page]).per(9)
     end
 
 end
