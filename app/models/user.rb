fe147ae22b06ec71af92e58dc8ce03afc9e67fd8
brian <geekgryphon@gmail.com>
2018-07-27 07:01:52 +0800

    finish like function and create followship model and set their relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,13 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
 
+
+  has_many :likes, dependent: :destroy
+  has_many :liked_restaurants, through: :likes, source: :restaurant
+
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships
+
   def admin?
     self.role == "admin"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180725221956) do
+ActiveRecord::Schema.define(version: 20180726225840) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,6 +27,20 @@ ActiveRecord::Schema.define(version: 20180725221956) do
   end
 
   create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "followships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "following_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "restaurant_id"
     t.datetime "created_at", null: false
